56e63612eb27e0ad513099d9f131bf18a83dd203
icefirex <icefire.hoek@gmail.com>
2012-06-19 10:26:46 +0200

    PLBerkelium-Example added

---
+++ PLBerkelium-Example/Main.cpp
@@ -0,0 +1,14 @@
+//[-------------------------------------------------------]
+//[ Includes                                              ]
+//[-------------------------------------------------------]
+#include <PLCore/Frontend/FrontendMain.h>
+
+
+//[-------------------------------------------------------]
+//[ Module definition                                     ]
+//[-------------------------------------------------------]
+pl_module_application("PLBerkelium-Example", "Application")
+	pl_module_vendor("The PixelLight Community")
+	pl_module_license("GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version")
+	pl_module_description("PLBerkelium-Example")
+pl_module_end
