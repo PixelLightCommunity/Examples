f1ec7cf406f39f6891ca5c37369b5cae8fe9b7f6
Christian Ofenberg <c.ofenberg@pixellight.org>
2012-07-01 13:57:51 +0200

    Added Visual Studio 2010 project files

--- PLBerkelium-Example/README.txt
+++ PLBerkelium-Example/README.txt
@@ -2,3 +2,20 @@ This example utilizes the PLBerkelium plugin, https://github.com/PixelLightCommu
 
 You can also use this example with the PLAwesomium plugin (https://github.com/PixelLightCommunity/Plugins/tree/master/PLAwesomium),
 this because PLBerkelium and PLAwesomium practically are identical in their methods.
+
+The examples are using relative paths. Clone the examples Git repository into the same directory you cloned the plugins Git repository into:
+- <my directory name>
+	- Examples
+		- Bin
+		- PLBerkelium-Example
+		- ...
+	- Plugins
+		- Bin
+		- PLBerkelium
+		- ...
+
+# Building under Windows using Visual Studio 2010 and the current Git version of PixelLight
+1. Add an environment variable "PL_ROOT" pointing to the location of your PixelLight Git repository (e.g. "C:\pixellight\")
+2. Add an environment variable "BERKELIUM_ROOT" pointing to the location of your Berkelium copy (e.g. "C:\berkelium\")
+3. Restart Visual Studio in case it's currently opened
+4. Open "PLBerkelium-Example.sln" and build it
---
+++ PLBerkelium-Example/src/Application.cpp
@@ -0,0 +1,81 @@
+//[-------------------------------------------------------]
+//[ Header                                                ]
+//[-------------------------------------------------------]
+#include "Application.h"
+
+
+//[-------------------------------------------------------]
+//[ Namespace                                             ]
+//[-------------------------------------------------------]
+using namespace PLCore;
+using namespace PLRenderer;
+using namespace PLScene;
+using namespace PLInput;
+
+
+//[-------------------------------------------------------]
+//[ RTTI interface                                        ]
+//[-------------------------------------------------------]
+pl_implement_class(Application)
+
+
+//[-------------------------------------------------------]
+//[ Functions		                                      ]
+//[-------------------------------------------------------]
+Application::Application(Frontend &cFrontend) : EngineApplication(cFrontend),
+	SlotOnControlEvent(this),
+	m_pGui(nullptr)
+{
+}
+
+
+Application::~Application()
+{
+	if (m_pGui) Berkelium::destroy(); // required
+}
+
+
+void Application::OnInit()
+{
+	EngineApplication::OnInit();
+
+	Controller *pController = reinterpret_cast<Controller*>(EngineApplication::GetInputController());
+	if (pController)
+	{
+		pController->SignalOnControl.Connect(SlotOnControlEvent);
+	}
+
+	const Class *pClass = ClassManager::GetInstance()->GetClass("PLBerkelium::Gui");
+	if (pClass)
+	{
+		m_pGui = (PLBerkelium::Gui*)pClass->Create();
+
+		m_pGui->SetRenderers(GetRenderer(), GetSceneRendererTool().GetSceneRenderer()); // required
+		m_pGui->ConnectEventUpdate(GetSceneContext()); // required
+
+		m_pGui->ConnectController(pController); // optional but recommended
+		m_pGui->SetMousePointerVisible(true); // optional
+		m_pGui->AddWindow("windows1", true, "http://dev.pixellight.org/forum/", 1024, 512, 32, 32); // optional
+		m_pGui->GetWindow("windows1")->SetToolTipEnabled(true); // optional
+	}
+}
+
+
+void Application::OnStop()
+{
+	EngineApplication::OnStop();
+
+	if (m_pGui) m_pGui->DestroyInstance(); // required
+}
+
+
+void Application::OnControlEvent(Control &cControl)
+{
+	if (cControl.GetType() == ControlButton && reinterpret_cast<Button&>(cControl).IsHit())
+	{
+		if (cControl.GetName() == "KeyboardEscape")
+		{
+			EngineApplication::Exit(0);
+		}
+	}
+}
